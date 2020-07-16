49121b66571dd793b5f9a52e31237c587b1a9053
Aj <ajaykuma24@gmail.com>
2020-07-16 17:49:45 +0200

    updated

---
+++ Nlp/Test2.py
@@ -0,0 +1,61 @@
+#Cleaning data
+# Let's take a look at our data again
+next(iter(data.keys()))
+
+# Notice that our dictionary is currently in key: comedian, value: list of text format
+next(iter(data.values()))
+
+# We are going to change this to key: comedian, value: string format
+def combine_text(list_of_text):
+    '''Takes a list of text and combines them into one large chunk of text.'''
+    combined_text = ' '.join(list_of_text)
+    return combined_text
+
+# Combine it!
+data_combined = {key: [combine_text(value)] for (key, value) in data.items()}
+
+# We can either keep it in dictionary format or put it into a pandas dataframe
+import pandas as pd
+pd.set_option('max_colwidth',150)
+
+data_df = pd.DataFrame.from_dict(data_combined).transpose()
+data_df.columns = ['transcript']
+data_df = data_df.sort_index()
+data_df
+
+# Let's take a look at the transcript for Ali Wong
+data_df.transcript.loc['ali']
+
+# Apply a first round of text cleaning techniques
+import re
+import string
+
+def clean_text_round1(text):
+    '''Make text lowercase, remove text in square brackets, remove punctuation and remove words containing numbers.'''
+    text = text.lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    return text
+
+round1 = lambda x: clean_text_round1(x)
+
+# Let's take a look at the updated text
+data_clean = pd.DataFrame(data_df.transcript.apply(round1))
+data_clean
+
+# Apply a second round of cleaning
+def clean_text_round2(text):
+    '''Get rid of some additional punctuation and non-sensical text that was missed the first time around.'''
+    text = re.sub('[‘’“”…]', '', text)
+    text = re.sub('\n', '', text)
+    return text
+
+round2 = lambda x: clean_text_round2(x)
+
+# Let's take a look at the updated text
+data_clean = pd.DataFrame(data_clean.transcript.apply(round2))
+data_clean
+
+
+
